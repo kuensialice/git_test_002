aab8093c9bff5457895c1f9616986ad638416eda
YinJiang <2171834185@qq.com>
2022-11-21 00:04:33 +0800

    push commit1 22/11/21 00:04

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello IDEA and Git!! 第一次修改");
         System.out.println("Hello IDEA and Git!! 第二次修改");
         System.out.println("Hello IDEA and Git!! 第三次修改");
+        System.out.println("Hello IDEA and Git!! Push Test! 第四次修改");
     }
 }
