426c1755cbb91f47cd54e70b64cc20c036b9a23e
YinJiang <2171834185@qq.com>
2022-11-20 23:05:08 +0800

    third commit 22/11/20 23:05

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello IDEA and Git!!");
         System.out.println("Hello IDEA and Git!! 第一次修改");
+        System.out.println("Hello IDEA and Git!! 第二次修改");
     }
 }
