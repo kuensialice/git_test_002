8dee13da78304136e15be6bb7f097a6f82a59dae
YinJiang <2171834185@qq.com>
2022-11-20 23:03:24 +0800

    second commit 22/11/20 23:03

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello IDEA and Git!!");
+        System.out.println("Hello IDEA and Git!! 第一次修改");
     }
 }
